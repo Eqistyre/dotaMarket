32de3609c382a33d4688b80eab3c465450f79b52
Eqistyre <stillalive@aliyun.com>
2016-04-08 14:15:14 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+gulp.task('watch', function(){
+
+});
+
+gulp.task();
+
+gulp.task('styles', () => {
+  return gulp.src('app/styles/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('default', []);
